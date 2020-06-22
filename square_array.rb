f808919c1609ff42a545ba4be42b251fb97d0caa
Christopher Dutcher <cadutcher@icloud.com>
2020-06-22 02:24:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,8 @@
 def square_array(numbers)
 new_numbers = []
 numbers.each do |number|
-  new_number = number * number
+  new_number = number**2
   new_numbers.push(new_number)
 end
 return new_numbers
 end
-
-square_array
